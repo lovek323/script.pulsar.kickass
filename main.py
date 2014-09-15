dcd2aec2cf70d833505962aca0abdf705c272168
mucky-duck <mucky-duck@users.noreply.github.com>
2014-09-15 23:12:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,17 @@ def search(query):
         data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
     return [{"uri": magnet} for magnet in re.findall(r'magnet:\?[^\'"\s<>\[\]]+', data)]
 
-
 def search_episode(imdb_id, tvdb_id, name, season, episode):
     return search("%s S%02dE%02d" % (name, season, episode))
 
 
 def search_movie(imdb_id, name, year):
-    return search(imdb_id)
+    response = urllib2.urlopen("http://katproxy.bz/usearch/" + name + "%20category:movies/")
+    data = response.read()
+    if response.headers.get("Content-Encoding", "") == "gzip":
+        import zlib
+        data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
+    return [{"uri": magnet} for magnet in re.findall(r'magnet:\?[^\'"\s<>\[\]]+', data)]
 
 urllib2.urlopen(
     PAYLOAD["callback_url"],
