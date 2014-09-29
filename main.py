9cece4680a197115613a916fbbef5a2a97802499
Jason "Don" O'Conal <lovek323@gmail.com>
2014-09-29 09:25:56 +0000

    add more logging, revert to katproxy, implement cache

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-import sys
-import json
 import base64
+import hashlib
+import json
+import os
 import re
+import sys
 import urllib
 import urllib2
+import xbmcaddon
 
-PAYLOAD    = json.loads(base64.b64decode(sys.argv[1]))
-PREFIX_LOG = 'KICKASS - '
+PAYLOAD      = json.loads(base64.b64decode(sys.argv[1]))
+PREFIX_LOG   = 'KICKASS - '
+__addon__    = xbmcaddon.Addon(str(sys.argv[0]))
+cache_prefix = xbmc.translatePath('special://temp') + __addon__.getAddonInfo('name').lower().replace(' ','_') + '_cache_'
 
 def search(query):
     print PREFIX_LOG + ("Running search query: %s" % query)
-    response = urllib2.urlopen("http://kickass.to/search?q=%s" % urllib.quote_plus(query))
-    data = response.read()
-    if response.headers.get("Content-Encoding", "") == "gzip":
-        import zlib
-        data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
+    url = "http://katproxy.bz/usearch/%s" % query
+    m = hashlib.md5()
+    m.update(url)
+    url_hash = m.hexdigest()
+    cache_file = cache_prefix + url_hash
+    print PREFIX_LOG + ("Cache file: %s" % cache_file)
+    if (os.path.isfile(cache_file)):
+        print PREFIX_LOG + "Pulling from cache"
+        f = open(cache_file, 'r')
+        data = f.read()
+        f.close()
+    else:
+        print PREFIX_LOG + ("Requesting: %s" % url)
+        response = urllib2.urlopen(url)
+        data = response.read()
+        if response.headers.get("Content-Encoding", "") == "gzip":
+            import zlib
+            data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
+        f = open(cache_file, 'w')
+        f.write(data)
+        f.close()
     magnets = re.findall(r'magnet:\?[^\'"\s<>\[\]]+', data)
     print PREFIX_LOG + ("Found %d magnet links" % len(magnets))
     return [{"uri": magnet} for magnet in magnets]
@@ -25,7 +46,7 @@ def search_episode(imdb_id, tvdb_id, name, season, episode):
 
 
 def search_movie(imdb_id, name, year):
-    response = urllib2.urlopen("http://kickass.to/usearch/" + name + "%20category:movies/")
+    response = urllib2.urlopen("http://katproxy.bz/usearch/" + name + "%20category:movies/")
     data = response.read()
     if response.headers.get("Content-Encoding", "") == "gzip":
         import zlib
@@ -36,5 +57,3 @@ urllib2.urlopen(
     PAYLOAD["callback_url"],
     data=json.dumps(globals()[PAYLOAD["method"]](*PAYLOAD["args"]))
 )
-
-
